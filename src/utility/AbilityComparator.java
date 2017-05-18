e8bacde94ac0b6e95b6985ed93727c0cf522ff6f
Joe Stewart <jstewart9@gmail.com>
2017-05-18 13:35:20 -0500

    Cleaned up and fixed code and javadoc.

--- src/utility/AbilityComparator.java
+++ src/utility/AbilityComparator.java
@@ -1,73 +1,52 @@
-/*
- * AbilityComparator - This class compares students based on 
- * their ability levels. There are three ability levels: low,
- * average, and high. The result is in ascending order with 
- * low being the lowest and high being the highest.
- * 
- * This class has been modified to accommodate cases in which
- * either of the two Student objects has a null value for the
- * abilityLevel.
- * 
- * Joseph Stewart, 2016
- */
-
 package utility;
 
 import java.util.Comparator;
 import model.*;
 
 /**
- * AbilityComparator - This class compares students based on 
- * their ability levels. There are three ability levels: low,
- * average, and high. The result is in ascending order with 
- * low being the lowest and high being the highest.
- * 
- * This class has been modified to accommodate cases in which
- * either of the two Student objects has a null value for the
- * abilityLevel.
+ * The AbilityComparator allows Students to be compared based on
+ *  their ability levels instead of their names (natural ordering).
  * 
  * @author Joseph Stewart
- * @version 1.2
  */
 public class AbilityComparator implements Comparator<Student> {
 
 	/**
 	 * This method takes in two students as parameters and compares
-	 * their ability levels.
+	 * them based on their ability levels.
 	 * 
 	 * @return The integer result of the comparison is returned.
 	 */	
 	@Override
 	public int compare(Student s1, Student s2) {
-		
-		AbilityLevel s1Ability = s1.getAbilityLevel();
-		AbilityLevel s2Ability = s2.getAbilityLevel();	
-		
 		int s1Value;
 		int s2Value;		
 		
-		// Assign value to s1
-		if(s1Ability == null) {
+		// Assign a value to s1Value
+		if(s1 == null || s1.getAbilityLevel() == null) {
 			s1Value = 0;
-		} else if(s1Ability.equals(AbilityLevel.HIGH)) {
-			s1Value = 3;
-		} else if(s1Ability.equals(AbilityLevel.AVERAGE)) {
-			s1Value = 2;
-		} else {
-			s1Value = 1;
-		}	
-				
-		// Assign value to s2
-		if(s2Ability == null) {
-			s2Value = 0;
-		} else if(s2Ability.equals(AbilityLevel.HIGH)) {
-			s2Value = 3;
-		} else if(s2Ability.equals(AbilityLevel.AVERAGE)) {
-			s2Value = 2;
-		} else {
-			s2Value = 1;
+		} else {			
+			if(s1.getAbilityLevel().equals(AbilityLevel.HIGH)) {
+				s1Value = 3;
+			} else if(s1.getAbilityLevel().equals(AbilityLevel.AVERAGE)) {
+				s1Value = 2;
+			} else {
+				s1Value = 1;
+			}			
 		}
-		
+				
+		// Assign a value to s2Value
+		if(s2 == null || s2.getAbilityLevel() == null) {
+			s2Value = 0;
+		} else {
+			if(s2.getAbilityLevel().equals(AbilityLevel.HIGH)) {
+				s2Value = 3;
+			} else if(s2.getAbilityLevel().equals(AbilityLevel.AVERAGE)) {
+				s2Value = 2;
+			} else {
+				s2Value = 1;
+			}		
+		}		
 		return s1Value - s2Value;		
 	}
 }
